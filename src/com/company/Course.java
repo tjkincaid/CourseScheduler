1859a685364c58d6b84dce8c4e1e4e1d2b1c5e70
tjkincaid <tjkincaid92@gmail.com>
2015-10-23 16:44:26 -0400

    Update Course.java

--- src/com/company/Course.java
+++ src/com/company/Course.java
@@ -7,12 +7,15 @@ import java.util.List;
  * Created by TJ on 10/22/15.
  */
 public class Course {
-    private String courseName;
-    private List<String> preReqs;
+    public String courseName;
+    public List<String> preReqs;
 
     public Course(String name, List<String> preReq)
     {
         this.courseName = name;
         this.preReqs = preReq;
     }
+
+
+
 }
